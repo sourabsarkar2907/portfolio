7e62ce4141a9f55b32d781bf72138ac385d7a22a
sourabsarkar2907 <101975530+sourabsarkar2907@users.noreply.github.com>
2023-02-27 11:41:16 +0530

    Add files via upload

---
+++ new_project.js
@@ -0,0 +1,9 @@
+
+console.log("script running... ")
+
+
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo')
+
+
+})
